77d9f07ecb4c4c6bfa33b83aa869a9995f03862c
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-01-22 19:36:45 -0800

    dv - tweaked movie css

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react'
 // Note that we don't need to import App.css since this component is a child of App.js
+// Assuming movie poster size has the ratio of 4050 pixels wide by 6000 pixels high
 
 function Movie(props) {
     const image_name = "Image of cover for " + props.title
+
+    function redirectMovie(e) {
+        e.preventDefault();
+        console.log("The movie poster was clicked");
+    }
+
     return(
         <div class="movie">
-            <img src={props.url} alt={image_name}/>
-            <div class="overlay">
-                <div class="movie_text">{props.title}</div>
-            </div>
+            <img src={props.url} alt={image_name} width="200" height="296"/>
+            <a href={props.url}>
+                <div class="overlay">
+                    <div class="movie_text">{props.title}</div>
+                </div>
+            </a>
         </div>
     )
 }
