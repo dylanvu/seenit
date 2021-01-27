6d5b3f1239dbe4c69ae5ee7242eef61417e70b2d
jenferp <77474432+jenferp@users.noreply.github.com>
2021-01-26 22:45:51 -0800

    tryna pull

---
+++ src/Components/LoginButton.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "./button.css"
+
+
+var React = require('react');
+
+var buttonStyle = {
+  margin: '10px 10px 10px 0'
+};
+
+var Button = React.createClass({
+  render: function () {
+    return (
+      <button
+        className="btn btn-default"
+        style={buttonStyle}
+        onClick={this.props.handleClick}>{this.props.label}</button>
+    );
+  }
+});
