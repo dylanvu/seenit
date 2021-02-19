498fcce772c0fcbad3184d87b81c95f3768f6e6c
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-02-18 23:31:33 -0800

    dv - Started to generalize MoviePage with props

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -5,25 +5,35 @@ import React from 'react'
 // Name (For movie title)
 // Summary? Director? Writer? Actors? Not sure how this will work. Likely depends on how the data we obtain is formatted.
 
+
 const MoviePage = () => {
+    // I've set up a temporary "props" example
+    let props = {
+        title: "La La Land",
+        summary: "While navigating their careers in Los Angeles, a pianist and an actress fall in love while attempting to reconcile their aspirations for the future.",
+        director: "Director",
+        writer: "Joe Writer",
+        actors: "Ryan Gosling"
+    }
+    
     return (
         <div className="MoviePageFlex">
             <div className="MovieImage">
                 <img src="https://images-na.ssl-images-amazon.com/images/I/61pVLV%2Bz11L._AC_SL1162_.jpg" alt="Movie Poster" className="MoviePagePoster" />
             </div>
             <div className="MovieContent">
-                <h1>La La Land</h1>
+                <h1>{props.title}</h1>
                 <p>
-                    While navigating their careers in Los Angeles, a pianist and an actress fall in love while attempting to reconcile their aspirations for the future.
+                    {props.summary}
                 </p>
                 <h2>Director:&nbsp;
-                    <span>Joe Director</span>
+                    <span>{props.director}</span>
                 </h2>
                 <h2>Writer:&nbsp;
-                    <span>Joe Writer</span>
+                    <span>{props.writer}</span>
                 </h2>
                 <h2>Starring:&nbsp;
-                    <span>Ryan Gosling</span>
+                    <span>{props.actors}</span>
                 </h2>
             </div>
         </div>
