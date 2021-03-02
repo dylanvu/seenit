0743cef6e4229b5124f47d1a5029fa09f764bba7
jenferp <77474432+jenferp@users.noreply.github.com>
2021-03-01 20:01:31 -0800

    searchpage and searchmovie debug

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -8,23 +8,25 @@ const SearchPage = () => {
     const [query, setQuery] = useState('');
     const [movies, setMovies] = useState([]);
     const [alert, setAlert] = useState('');
-
+    
     const API_KEY = "78fcb8fa5df23ceee859f6258985efc4";
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}'; // version 3
+    const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=' + query; // version 3
 
     const getData = async () => {
         if (query !== "") {
-            const result = await Axios.get(url); // fetches the data
-            if (result.data.results == []) { // alert if results array is empty
+            const result = await Axios.get(SEARCH_API); // fetches the data
+            if (result.results == []) { // alert if results array is empty
                 return setAlert("No movie with such name");
             }
-            console.log(result);
+            console.log(result.data.results);
+            console.log(result.data.results[0])
             setMovies(result.data.results); // get the movies data using results array
             setQuery("");
             setAlert("");
         } else {
             setAlert("Please fill out the form")
         }
+        return null;
     };
 
     // Set the query so that if updates with each search
@@ -54,8 +56,8 @@ const SearchPage = () => {
             </form>
             {/* list of movies displayed */}
             <div className="search-results-container">
-                {movies != [] && // check if movies isn't empty
-                    movies.map(movie => <SearchMovie key={movie.id} {...movie} />)}
+                {movies !== [] &&  // check if movies isn't empty, map to get movie, create a SearchMovie for each movie
+                    movies.map((movie) => <SearchMovie key={movie.id} {...movie} />)} 
             </div>
         </div>
     );
